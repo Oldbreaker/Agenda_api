4f6b682a4b0983ac1355dbb1228f7233e65f4208
Fernando Valenzuela Ramos <amdati5000@gmail.com>
2021-07-06 01:00:39 -0500

    Api view list and create

--- agenda/applications/persona/views.py
+++ agenda/applications/persona/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+from rest_framework.generics import ListAPIView,CreateAPIView
+
+from .models import Person
+
+from .serializers import PersonSerializer
 
 
+class ListaPersonas(ListView):
+    template_name = "persona/personas.html"
+    context_object_name='personas'
+    
+    def get_queryset(self):
+        return Person.objects.all()
+    
+    
+
+class PersonListApiView(ListAPIView):
+    
+    serializer_class=PersonSerializer
+    
+    def get_queryset(self):
+        return Person.objects.all()
+
+class PersonCreateView(CreateAPIView):
+    
+    serializer_class=PersonSerializer
+    
+    
+    
 
